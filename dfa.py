bff85b9f56926b509411811bbfe3849697bbf11f
IonianIronist <36776432+IonianIronist@users.noreply.github.com>
2020-04-08 01:03:46 +0300

    solution

--- dfa.py
+++ dfa.py
@@ -1,82 +1,61 @@
-"""
-Κώδικας που θα χρησιμοποιηθεί ως βάση για την 1η εργασία
-των Μεταγλωττιστών (αναγνώριση κειμένου μέσω αυτομάτου DFA).
-
-ΠΡΟΣΟΧΗ: Προσθήκες στον κώδικα επιτρέπονται μόνο
-στα σημεία (Α), (Β) και (Γ) - διαβάστε τα σχόλια!
-"""
-
-
-transitions = { 
-
-	# (Α) Συμπληρώστε τον πίνακα μεταβάσεων ως λεξικό (dictionary).
-	# Η αρχική κατάσταση πρέπει να ονομάζεται 's0'.
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id7
-
-     	      } 
+def get_char(text, pos):
+    if pos < 0 or pos >= len(text):
+        return None
+    c = text[pos]
+    if '0' <= c <= '9':
+        return "int"
+    elif c is ".":
+        return "dot"
 
 
-accepts = { 
-
-	# (Β) Συμπληρώστε το λεξικό των καταστάσεων αποδοχής και των
-	# αντίστοιχων επιστρεφόμενων συμβόλων (tokens)
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id8
-
-     	  }
-
-
-def get_char(text,pos):
-	""" Returns char (or char category) at position `pos` of `text`,
-	or None if out of bounds. """
-	
-	if pos<0 or pos>=len(text): return None
-	
-	c = text[pos]
-	
-	# (Γ) Προαιρετικά, μπορείτε να ομαδοποιήσετε τους
-	# χαρακτήρες εισόδου εδώ.
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id11
-	
-	return c
-	
-
-# Δεν επιτρέπεται η παρέμβαση στον κώδικα από αυτό το σημείο και κάτω! 
-
-def scan(text,transitions,accepts,state):
-	""" Starting from inital `state`, scans `text` while transitions
-	exist in `transitions` dict. After that, if on a state belonging to
-	`accepts` dict, returns the corresponding token object, else None.
-	"""
-	
-	# initial position on text
-	pos = 0
-	
-	# memory object for last seen accepting state
-	matched = None
-	
-	while True:
-		
-		c = get_char(text,pos)	# get next char (or char category)
-		
-		if state in transitions and c in transitions[state]:
-			state = transitions[state][c]	# set new state
-			pos += 1	# advance to next char			
-
-			# remember if current state is accepting
-			if state in accepts:
-				matched = { 'token':  accepts[state],
-					    'lexeme': text[:pos] }
-			
-		else:	# no transition found, return last match or None
-			return matched
-			
-
-# testing inputs
-for test in ['12.456','6789.','.66998','1234','.']:
-	m = scan(test,transitions,accepts,'s0')
-	print("Testing '{}'\nResult: {}\n".format(test,m))
+def scan(text, transitions, accepts, start):
+    pos = 0
+    state = start
+    matched = None
+    while True:
+        c = get_char(text, pos)
+        if state in transitions and c in transitions[state]:
+            state = transitions[state][c]
+            pos += 1
+            if state in accepts:
+                matched = {"lexeme": text[:pos],
+                           "token": accepts[state],
+                           }
+        else:
+            return matched
 
 
+transitions = {"s0": {"dot": "s1", "int": "s2"},
+               "s1": {"int": "s3"},
+               "s2": {"int": "s2", "dot": "s4"},
+               "s3": {"int": "s3"},
+               "s4": {"int": "s3"},
+               }
+
+accepts = {
+    "s3": "FLOAT_TOKEN",
+    "s4": "FLOAT_TOKEN",
+}
+
+for test in ['12.456', '6789.', '.66998', '1234', '.']:
+    m = scan(test, transitions, accepts, 's0')
+    print("Testing '{}'\nResult: {}\n".format(test, m))
+
+# I think You were going easy on us :)
+print("""
+⊂_ヽ
+　 ＼＼ (p2017182)
+　　 ＼( ͡° ͜ʖ ͡°)
+　　　 >　⌒ヽ
+　　　/ 　 へ＼
+　　 /　　/　＼＼
+　　 ﾚ　ノ　　 ヽ_つ
+　　/　/
+　 /　/|
+　(　(ヽ
+　|　|、＼
+　| 丿 ＼ ⌒)
+　| |　　) /
+ノ )　　Lﾉ
+(_／
+""")
